11a9c5c96f492ef8f4ca7e30b418ee36574b70bb
Demere Walker <blessedtrulywalker32@gmail.com>
2023-11-21 09:24:12 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Meditation from './Meditation'; // Import the Meditation component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mindfulness and Wellness App</h1>
       </header>
+      <main>
+        <p>Welcome to your personal mindfulness space.</p>
+        {/* Other components and content will go here */}
+      </main>
     </div>
   );
 }
---
+++ src/Meditation.js
@@ -0,0 +1,14 @@
+import React from 'react'; // Import React
+
+function Meditation() {
+  // The Meditation component
+  return (
+    <div>
+      {/* A container for the Meditation component */}
+      <h2>Meditation Practices</h2> 
+      {/* More content will be added here */}
+    </div>
+  );
+}
+
+export default Meditation; // Export the component for use in other files
